ae35fe8606297e02221c219392a24fb745e11a1a
Jeff Windsor <jeff.windsor@gmail.com>
2021-09-26 22:19:35 -0700

    polybar xmonad

---
+++ xmonad/.xmonad/polybar/launch.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
+
+# load bars
+desktop=$(echo $DESKTOP_SESSION)
+count=$(xrandr --query | grep " connected" | cut -d" " -f1 | wc -l)
+if [ $count = 1 ]; then
+  m=$(xrandr --query | grep " connected" | cut -d" " -f1)
+  MONITOR=$m polybar --reload topbar -c ~/.xmonad/polybar/config &
+  #MONITOR=$m polybar --reload bottombar -c ~/.xmonad/polybar/config &
+else
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload topbar -c ~/.xmonad/polybar/config &
+    #MONITOR=$m polybar --reload bottombar -c ~/.xmonad/polybar/config &
+  done
+fi
