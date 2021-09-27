6ea97f9f42b9c82be8b58234cc0f0b5707b2305d
Jeff Windsor <jeff.windsor@gmail.com>
2021-09-27 08:07:42 -0700

    battery and bluetooth start

--- xmonad/.xmonad/scripts/autostart.sh
+++ xmonad/.xmonad/scripts/autostart.sh
@@ -26,7 +26,7 @@ run pamac-tray &
 run xfce4-power-manager &
 run volumeicon &
 #numlockx on &
-blueberry-tray &
+#blueberry-tray &
 picom --config $HOME/.xmonad/scripts/picom.conf &
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 /usr/lib/xfce4/notifyd/xfce4-notifyd &
