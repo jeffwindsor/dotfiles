29056a7b44e98c32c03faee0f448014cec18ca11
Jeff Windsor <jeff.windsor@gmail.com>
2021-09-28 08:23:07 -0700

    move polybar to independent package

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -11,11 +11,11 @@ desktop=$(echo $DESKTOP_SESSION)
 count=$(xrandr --query | grep " connected" | cut -d" " -f1 | wc -l)
 if [ $count = 1 ]; then
   m=$(xrandr --query | grep " connected" | cut -d" " -f1)
-  MONITOR=$m polybar --reload topbar -c ~/.xmonad/polybar/config &
-  #MONITOR=$m polybar --reload bottombar -c ~/.xmonad/polybar/config &
+  MONITOR=$m polybar --reload topbar -c ~/.config/polybar/config &
+  #MONITOR=$m polybar --reload bottombar -c ~/.config/polybar/config &
 else
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload topbar -c ~/.xmonad/polybar/config &
-    #MONITOR=$m polybar --reload bottombar -c ~/.xmonad/polybar/config &
+    MONITOR=$m polybar --reload topbar -c ~/.config/polybar/config &
+    #MONITOR=$m polybar --reload bottombar -c ~/.config/polybar/config &
   done
 fi
--- xmonad/.xmonad/scripts/autostart.sh
+++ xmonad/.xmonad/scripts/autostart.sh
@@ -11,7 +11,7 @@ function run {
 xrandr --output eDP-1 --mode 1920x1080 --pos 0x1080 --rotate normal --output HDMI-1 --primary --mode 3840x2160 --pos 1920x0 --rotate normal --output DP-1 --off --output HDMI-2 --off
 
 # polybar 
-(sleep 2; run $HOME/.xmonad/polybar/launch.sh) &
+(sleep 2; run $HOME/.config/polybar/launch.sh) &
 
 # cursor active at boot
 xsetroot -cursor_name left_ptr &
