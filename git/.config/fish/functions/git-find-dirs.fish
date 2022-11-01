f4f230379c9b04e4744d1ebf42c6fe204b2e521b
Jeff Windsor <jeff.windsor@gmail.com>
2022-11-01 16:48:42 -0700

    Refactor

--- git/.config/fish/functions/git-find-dirs.fish
+++ git/.config/fish/functions/git-find-dirs.fish
@@ -1,4 +1,4 @@
 function git-find-dirs
   # find all directories ending in .git (including hidden and ignored listings)
-  fd '^.git$' $argv[1] -HItd --max-depth 4 | sed 's/.git\/$//'
+  fd '^.git$' $argv[1] -HItd --max-depth 4 | sed 's/.git$//'
 end
