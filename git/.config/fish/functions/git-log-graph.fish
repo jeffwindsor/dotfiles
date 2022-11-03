6f9663fc360f08d54a48bba3bd9cb6ea8bbd9d1e
Windsor, Jeffrey <jwindsor@cj.com>
2022-11-03 08:33:11 -0700

    fix outcome of test -z

--- git/.config/fish/functions/git-log-graph.fish
+++ git/.config/fish/functions/git-log-graph.fish
@@ -1,7 +1,7 @@
 function git-log-graph
   if test -z $argv[1]
-    git log --graph --pretty=format:$GIT_LOG_PRETTY_FORMAT --abbrev-commit --max-count=$argv[1]
-  else
     git log --graph --pretty=format:$GIT_LOG_PRETTY_FORMAT --abbrev-commit --max-count=10
+  else
+    git log --graph --pretty=format:$GIT_LOG_PRETTY_FORMAT --abbrev-commit --max-count=$argv[1]
   end
 end
