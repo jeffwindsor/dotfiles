1b1ea4d4a82723b7a2d720479ad8a1ed0c8147ea
Jeff Windsor <jeff.windsor@gmail.com>
2022-11-28 18:44:05 -0800

    Refactor

--- nvim/.config/nvim/plugin/lualine.lua
+++ nvim/.config/nvim/plugin/lualine.lua
@@ -5,8 +5,8 @@ require('lualine').setup {
   options = {
     icons_enabled = true,
     theme = 'auto',
-    component_separators = { left = '', right = ''},
-    section_separators = { left = '', right = ''},
+    component_separators =  { left = '', right = ''}, --{ left = '', right = ''},
+    section_separators = { left = '', right = ''}, --{ left = '', right = ''},
     always_divide_middle = true,
     globalstatus = true,
     refresh = {
