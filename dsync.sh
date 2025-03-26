db82b72ba58849fcf55bae58e763dba8765c5fcd
Jeff Windsor <jefwinds@WKMZTAFD6544.global.publicisgroupe.net>
2025-03-25 17:11:51 -0700

    stow by command existence

---
+++ dsync.sh
@@ -0,0 +1,45 @@
+#! /usr/bin/env bash
+source="${1:-$DOTFILES}"
+target="${2:-$HOME}"
+
+# map package name to command, for later overrides
+overrides=(
+  "helix;hx"
+  "nixpkgs;nix"
+  "nushell;nu"
+  "ripgrep;rg"
+  "television;tv"
+)
+
+echo "Source: $source"
+echo "Target: $target"
+
+# for each directory in source
+for path in $(fd -td -d1 "" "$source"); do
+  package=$(basename $path)
+  
+  # default values
+  check_command="$package"
+  
+  # check for override match
+  for o in "${overrides[@]}"; do
+    split=(${o//;/ }) # split override entry by semi-colon
+    
+    # look for match, override command if found
+    if [[ "$package" == "${split[0]}" ]]; then
+      check_command="${split[1]}"
+      break
+    fi
+   
+  done;
+
+  # only stow packages where command is active
+  if command -v "$check_command" &>/dev/null; then
+    echo -e "\e[0;32m $package: Clean and Restore $package \e[0m"
+    stow -S --dir="$source" --target="$target" "$package"
+  else
+    echo -e "\e[0;31m $package: $check_command not found, Clean only \e[0m"
+    stow -D --dir="$source" --target="$target" "$package"
+  fi
+done;
+
