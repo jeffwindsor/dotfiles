e3ae1dcaceb110d850117c222716e9ebb5473e85
Jeff Windsor <jeff.windsor@gmail.com>
2025-04-04 21:56:36 -0700

    REMOVE need for override
    Naming the stow package the same as the installed command, removes the
    need for overrides

--- dsync.sh
+++ dsync.sh
@@ -2,15 +2,6 @@
 source="${1:-$DOTFILES}"
 target="${2:-$HOME}"
 
-# map package name to command, for later overrides
-overrides=(
-  "helix;hx"
-  "nixpkgs;nix"
-  "nushell;nu"
-  "ripgrep;rg"
-  "television;tv"
-  "ghostty;ls"  # cannot seem to find ghostty in cli, so just punting with a known commands
-)
 echo -e "\e[94m== Dotfile Sync ==\e[0m"
 echo "Source: $source"
 echo "Target: $target"
@@ -19,27 +10,12 @@ echo "Target: $target"
 for path in $(fd -td -d1 "" "$source"); do
   package=$(basename $path)
   
-  # default values
-  check_command="$package"
-  
-  # check for override match
-  for o in "${overrides[@]}"; do
-    split=(${o//;/ }) # split override entry by semi-colon
-    
-    # look for match, override command if found
-    if [[ "$package" == "${split[0]}" ]]; then
-      check_command="${split[1]}"
-      break
-    fi
-   
-  done;
-
-  # only stow packages where command is active
-  if command -v "$check_command" &>/dev/null; then
-    echo -e "\e[0;32m $package: Clean and Restore $package \e[0m"
+  # only sync packages where package's command is installed
+  if command -v "$package" &>/dev/null; then
+    echo -e "\e[0;32m $package: synced \e[0m"
     stow -S --dir="$source" --target="$target" "$package"
   else
-    echo -e "\e[0;31m $package: $check_command not found, Clean only \e[0m"
+    echo -e "\e[0;31m $package: not installed \e[0m"
     stow -D --dir="$source" --target="$target" "$package"
   fi
 done;
