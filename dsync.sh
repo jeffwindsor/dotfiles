f50b182e158ef7d2b347b2f9962ad85a0719ce4d
Windsor, Jeff <jwindsor@cj.com>
2025-05-05 15:38:45 -0700

    Minor fixes to remove fd and sd

--- dsync.sh
+++ dsync.sh
@@ -7,16 +7,17 @@ echo "Source: $source"
 echo "Target: $target"
 
 # for each directory in source
-for path in $(fd -td -d1 "" "$source"); do
-  package=$(basename $path)
-  
-  # only sync packages where package's command is installed
-  if command -v "$package" &>/dev/null; then
-    echo -e "\e[0;32m $package: synced \e[0m"
-    stow -S --dir="$source" --target="$target" "$package"
-  else
-    echo -e "\e[0;31m $package: not installed \e[0m"
-    stow -D --dir="$source" --target="$target" "$package"
-  fi
-done;
+# for path in $(fd -td -d1 "" "$source"); do
 
+for path in $(find "$source" -maxdepth 1 -type d); do
+	package=$(basename $path)
+
+	# only sync packages where package's command is installed
+	if command -v "$package" &>/dev/null; then
+		echo -e "\e[0;32m $package: synced \e[0m"
+		stow -S --dir="$source" --target="$target" "$package"
+	else
+		echo -e "\e[0;31m $package: not installed \e[0m"
+		stow -D --dir="$source" --target="$target" "$package"
+	fi
+done
