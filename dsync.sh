1feadfb824a9af2e36fd7cd968acecae34e860f0
Windsor, Jeff <jwindsor@cj.com>
2025-05-05 22:18:02 -0700

    DSYNC with new colors

--- dsync.sh
+++ dsync.sh
@@ -2,22 +2,21 @@
 source="${1:-$DOTFILES}"
 target="${2:-$HOME}"
 
-echo -e "\e[94m== Dotfile Sync ==\e[0m"
-echo "Source: $source"
-echo "Target: $target"
+echo -e "${BG_LIGHTBLUE}${FG_BLACK}== Dotfile Sync ==${NC}"
+echo -e "${BG_GRAY}${FG_BLACK} Source ${NC}${FG_GRAY} $source${NC}"
+echo -e "${BG_GRAY}${FG_BLACK} Target ${NC}${FG_GRAY} $target${NC}"
 
-# for each directory in source
+# for each non-hidden directory in source
 # for path in $(fd -td -d1 "" "$source"); do
-
-for path in $(find "$source" -maxdepth 1 -type d); do
-	package=$(basename $path)
+for path in $(find "$source" -mindepth 1 -maxdepth 1 -type d -not -path "*/\.*"); do
+	package=$(basename "$path")
 
 	# only sync packages where package's command is installed
 	if command -v "$package" &>/dev/null; then
-		echo -e "\e[0;32m $package: synced \e[0m"
+		echo -e "${FG_LIGHTGREEN} $package: synced ${NC}"
 		stow -S --dir="$source" --target="$target" "$package"
 	else
-		echo -e "\e[0;31m $package: not installed \e[0m"
+		echo -e "${FG_GRAY} $package: not installed ${NC}"
 		stow -D --dir="$source" --target="$target" "$package"
 	fi
 done
