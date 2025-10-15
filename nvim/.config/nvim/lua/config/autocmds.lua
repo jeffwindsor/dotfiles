349e8b4206cfbc1615452ba0e5403f67d9b1f262
Windsor, Jeff <jwindsor@cj.com>
2025-10-15 16:25:36 -0700

    NVIM make file picker replace netrw

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -6,3 +6,22 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- when nvim is opened with a directory, use dir as root then open the file picker
+vim.api.nvim_create_autocmd("VimEnter", {
+	callback = function()
+		local args = vim.fn.argv()
+		if #args > 0 then
+			local arg = args[1]
+			if vim.fn.isdirectory(arg) == 1 then
+				-- The vim.schedule() wrapper ensures the picker opens after Neovim fully initializes
+				vim.schedule(function()
+					-- make directory the root
+					vim.cmd.cd(vim.fn.fnameescape(arg))
+					-- open a file picker in that directory
+					require("snacks").picker.files()
+				end)
+			end
+		end
+	end,
+})
--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -1,3 +1,7 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Disable netrw
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
