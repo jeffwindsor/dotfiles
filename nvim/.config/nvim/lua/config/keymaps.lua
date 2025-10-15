a43c5de6d092c70b62730319a4636fb49f8d339e
Windsor, Jeff <jwindsor@cj.com>
2025-10-14 20:44:33 -0700

    NVIM back to nvim

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Sort selection
+vim.keymap.set("v", "<leader><Down>", ":sort<cr>", { desc = "Sort Selection" })
+vim.keymap.set("v", "<leader><Up>", ":sort!<cr>", { desc = "Sort Selection Reversed" })
