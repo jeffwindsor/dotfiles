3374496f7a5597a75c6dae52c148548faeabacef
Windsor, Jeff <jwindsor@cj.com>
2025-11-21 09:55:29 -0800

    Themes

--- nvim/.config/nvim/lua/plugins/themes.lua
+++ nvim/.config/nvim/lua/plugins/themes.lua
@@ -15,11 +15,11 @@ return {
 		lazy = false,
 		priority = 1000,
 		opts = { options = { dim_inactive = true } },
-		config = function(_, opts)
-			require("nightfox").setup(opts)
-			vim.cmd.colorscheme("duskfox")
-			vim.cmd.highlight({ "WinSeparator", "guifg=#54546d" })
-		end,
+		-- config = function(_, opts)
+		-- 	require("nightfox").setup(opts)
+		-- 	vim.cmd.colorscheme("duskfox")
+		-- 	vim.cmd.highlight({ "WinSeparator", "guifg=#54546d" })
+		-- end,
 	},
 	{
 		"rebelot/kanagawa.nvim",
@@ -39,4 +39,10 @@ return {
 	{ "savq/melange-nvim" },
 	{ "sainnhe/everforest" },
 	{ "navarasu/onedark.nvim" },
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			colorscheme = "melange",
+		},
+	},
 }
