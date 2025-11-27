0ab365986cf899525660a403c1b52a90f4ccb5e8
Jeff Windsor <jeff.windsor@gmail.com>
2025-11-26 16:09:23 -0800

    NVIM Tidy.nvim to remove trailing spaces

---
+++ nvim/.config/nvim/lua/plugins/tidy_nvim.lua
@@ -0,0 +1,13 @@
+return {
+	{
+		"mcauley-penney/tidy.nvim",
+		opts = {
+			enabled_on_save = false,
+			filetype_exclude = { "markdown", "diff" },
+		},
+		init = function()
+			vim.keymap.set("n", "<leader>tt", require("tidy").toggle, {})
+			vim.keymap.set("n", "<leader>tr", require("tidy").run, {})
+		end,
+	},
+}
