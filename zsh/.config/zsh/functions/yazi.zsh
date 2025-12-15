0f985a7bea1074469e2c3e2cd290023778090547
Windsor, Jeff <jwindsor@cj.com>
2025-12-15 15:35:04 -0800

    ZSH Yasi wrapper as 'y'

--- zsh/.config/zsh/functions/yazi.zsh
+++ zsh/.config/zsh/functions/yazi.zsh
@@ -6,7 +6,7 @@
 # ═══════════════════════════════════════════════════
 
 # Yazi with directory change support
-yy() {
+yazi-cwd() {
   local tmp="$(mktemp -t "yazi-cwd.XXXXXX")"
   yazi "$@" --cwd-file="$tmp"
   if local cwd="$(cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
