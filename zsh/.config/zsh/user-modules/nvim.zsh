2d7960eccffc18345ea400d8f0ea6b9b32374f14
Windsor, Jeff <jwindsor@cj.com>
2026-01-23 12:58:15 -0800

    ZSH load correct

--- zsh/.config/zsh/user-modules/nvim.zsh
+++ zsh/.config/zsh/user-modules/nvim.zsh
@@ -1,18 +1,8 @@
 #!/usr/bin/env zsh
-nvim-by-appname() {
-  local config=$(fd --max-depth 1 --glob 'nvim-*' ~/.config | fzf --prompt=" " --height=~50% --layout=reverse --border --exit-0)
-  [[ -z $config ]] && echo "No config selected" && return
-  NVIM_APPNAME=$(basename $config) nvim $@
-}
-
 alias v='nvim'
 alias v.='nvim .'
-alias vl='NVIM_APPNAME=nvim-lazy nvim'
-alias vl.='NVIM_APPNAME=nvim-lazy nvim .'
-alias vm='NVIM_APPNAME=nvim-mini nvim'
-alias vm.='NVIM_APPNAME=nvim-mini nvim .'
-alias vv='nvim-by-appname'
-alias vv.='nvim-by-appname .'
+alias vm='NVIM_APPNAME=mini nvim'
+alias vm.='NVIM_APPNAME=mini nvim .'
 
 alias md='cd ~/markdown/'
 alias mde='cd ~/markdown/ && nvim'
