517402f7453878edb6667bd39b2415e0d7b446ca
Windsor, Jeff <jwindsor@cj.com>
2026-01-27 15:47:04 -0800

    NVIM Obsidian simplify

--- nvim/.config/nvim/lua/plugins/obsidian.lua
+++ nvim/.config/nvim/lua/plugins/obsidian.lua
@@ -3,17 +3,37 @@ return {
 	version = "*", -- use latest release, remove to use latest commit
 	ft = "markdown",
 	keys = {
-		{ "<leader>o", "<cmd>Obsidian<CR>", desc = "Obsidian Index" },
+		{ "<leader>o", "<cmd>Obsidian<CR>", desc = "Obsidian" },
+		{ "<leader>ob", "<cmd>Obsidian backlinks<CR>", desc = "Show backlinks" },
+		{ "<leader>oT", "<cmd>Obsidian tags<CR>", desc = "Search tags" },
+		{ "<leader>ol", "<cmd>Obsidian links<CR>", desc = "Show links" },
+		{ "<leader>on", "<cmd>Obsidian new<CR>", desc = "New" },
+		{ "<leader>oq", "<cmd>Obsidian quick-switch<CR>", desc = "Quick switch" },
+		{ "<leader>or", "<cmd>Obsidian rename<CR>", desc = "Rename" },
+		{ "<leader>oo", "<cmd>Obsidian search<CR>", desc = "Search" },
 	},
-	---@module 'obsidian'
-	---@type obsidian.config
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-treesitter/nvim-treesitter",
+		"saghen/blink.cmp",
+	},
 	opts = {
-		legacy_commands = false, -- this will be removed in the next major release
+		-- Specify vault location (required)
 		workspaces = {
 			{
 				name = "default",
 				path = "~/markdown",
 			},
 		},
+
+		-- PARA-specific: store images in attachments/ instead of default assets/imgs/
+		attachments = {
+			img_folder = "attachments",
+		},
+
+		-- macOS-specific: use 'open' command instead of default xdg-open (Linux)
+		follow_url_func = function(url)
+			vim.fn.jobstart({ "open", url })
+		end,
 	},
 }
