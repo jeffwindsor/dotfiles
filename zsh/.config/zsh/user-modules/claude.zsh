14e5b321e3807a2236591bf0ae1e5f5745379e27
Windsor, Jeff <jeffrey.windsor@cj.com>
2026-02-17 16:31:57 -0800

    CLAUDE back to Mircea Script

--- zsh/.config/zsh/user-modules/claude.zsh
+++ zsh/.config/zsh/user-modules/claude.zsh
@@ -3,30 +3,33 @@
 # ═══════════════════════════════════════════════════
 # FUNCTIONS
 # ═══════════════════════════════════════════════════
-# Open Dev Mux in a repo
-claude-dev() {
-  local source=$(tv git-repos)
-  cd "$source"
-  zellij --layout claude
-}
 
 # Claude Bedrock configuration
 claude-bedrock-direct() {
   export ANTHROPIC_MODEL="sonnet"
   export ANTHROPIC_SMALL_FAST_MODEL="haiku"
 
-  echo "node required for claude code"
-  mise ls node
+  echo "setting models to $ANTHROPIC_MODEL : $ANTHROPIC_SMALL_FAST_MODEL"
+  ~/.config/claude/bedrock.sh
+}
+
+claude-bedrock-direct-opus() {
+  export ANTHROPIC_MODEL="opus"
+  export ANTHROPIC_SMALL_FAST_MODEL="haiku"
 
   echo "setting models to $ANTHROPIC_MODEL : $ANTHROPIC_SMALL_FAST_MODEL"
   ~/.config/claude/bedrock.sh
 }
+
 
 # ═══════════════════════════════════════════════════
 # ALIASES
 # ═══════════════════════════════════════════════════
 alias aa='claude'
 alias a='claude-dev'
-alias cb='~/bin/claudecode'
-alias claude-bedrock='~/bin/claudecode'
+alias cb='claude-bedrock-direct'
+alias cbo='claude-bedrock-direct-opus'
+alias cbb='~/bin/claudecode'
+#alias cb='~/bin/claudecode'
+#alias claude-bedrock='~/bin/claudecode'
 
