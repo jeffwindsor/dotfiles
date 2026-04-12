07b80bd9030ca4894383e2d88178026ac8e262ad
Jeff Windsor <jeff.windsor@gmail.com>
2026-04-12 15:30:29 -0700

    NVIM minimax added

---
+++ nvim/.config/nvim-lazyvim/lua/plugins/_disable_plugins.lua
@@ -0,0 +1,9 @@
+return {
+	{ "MagicDuck/grug-far.nvim", enabled = false },
+	-- { "MunifTanjim/nui.nvim", enabled = false },
+	{ "folke/noice.nvim", enabled = false },
+	-- { "folke/persistence.nvim", enabled = false },
+	{ "folke/trouble.nvim", enabled = false },
+	{ "nvim-mini/mini.pairs", enabled = false },
+	{ "windwp/nvim-ts-autotag", enabled = false },
+}
--- nvim/.config/zsh/user-modules/nvim.zsh
+++ nvim/.config/zsh/user-modules/nvim.zsh
@@ -1,8 +1,8 @@
 #!/usr/bin/env zsh
-alias v='nvim'
-alias v.='nvim .'
-alias vm='NVIM_APPNAME=mini nvim'
-alias vm.='NVIM_APPNAME=mini nvim .'
+alias v='NVIM_APPNAME=nvim-lazyvim nvim'
+alias v.='NVIM_APPNAME=nvim-lazyvim nvim .'
+alias vm='NVIM_APPNAME=nvim-minimax nvim'
+alias vm.='NVIM_APPNAME=nvim-minimax nvim .'
 
 alias md='cd ~/markdown/projects/'
 alias mde='cd ~/markdown/projects/ && nvim'
