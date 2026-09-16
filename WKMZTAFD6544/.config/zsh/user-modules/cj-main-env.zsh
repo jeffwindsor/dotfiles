c4ba96db63a2a4133a801cd4e20bc0943eb469c5
Windsor, Jeff <jwindsor@cj.com>
2026-09-15 17:11:07 -0700

    CJ update the way we run devdb

---
+++ WKMZTAFD6544/.config/zsh/user-modules/cj-main-env.zsh
@@ -0,0 +1,13 @@
+#!/usr/bin/env zsh
+
+export MAVEN_OPTS="-Xss3m -XX:ReservedCodeCacheSize=256m"
+export SBT_OPTS='-Xms2G -Xmx2G'
+export JAVA_OPTS='-Xms4G -Xmx4G -XX:MetaspaceSize=96M -XX:MaxMetaspaceSize=2096M'
+export RESIN_HOME=$HOME/.local/bin/resin-4.0.66
+
+MAIN=$SOURCE/gitlab.cj.dev/cjdev/main
+BUILD_SCRIPTS=$MAIN/bin/build/maven
+RESIN_SCRIPTS=$MAIN/bin/resin
+RESIN_BIN=$RESIN_HOME/bin
+path=("$BUILD_SCRIPTS" "$RESIN_SCRIPTS" "$RESIN_BIN" $path)
+export PATH
